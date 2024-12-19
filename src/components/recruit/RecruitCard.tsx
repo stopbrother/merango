0ee9bbef6b648f405f4aeed72ab5109f527718ef
stopbrother <stopbro0323@naver.com>
2024-12-19 16:02:42 +0900

    refactor: 카드컴포넌트 분리리

---
+++ src/components/recruit/RecruitCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardFooter, CardHeader, CardTitle } from '../ui/card';
+import { Badge } from '../ui/badge';
+import { RecruitWithProfile } from '@/types/parties.types';
+
+interface RecruitCardProps {
+  recruit: RecruitWithProfile;
+}
+const RecruitCard = ({ recruit }: RecruitCardProps) => {
+  return (
+    <Card key={recruit.id} className="hover:shadow-lg cursor-pointer">
+      <CardHeader className="w-full flex flex-row justify-between items-center">
+        <Badge>{recruit.party_type}</Badge>
+        <span className="text-sm text-gray-500 !mt-0">
+          {recruit.created_date_time}
+        </span>
+      </CardHeader>
+      <CardTitle className="text-center">{recruit.title}</CardTitle>
+      <CardFooter className="justify-center">
+        파티장: {recruit.created_by.username}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default RecruitCard;
--- src/components/recruit/RecruitList.tsx
+++ src/components/recruit/RecruitList.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { useRecruitQuery } from '@/query/useRecruitQuery';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '../ui/card';
-import { Badge } from '../ui/badge';
+import RecruitCard from './RecruitCard';
 
 const RecruitList = () => {
   const { data, error } = useRecruitQuery();
@@ -18,22 +11,13 @@ const RecruitList = () => {
   console.log('data', data);
 
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {data?.map((recruit) => (
-        <Card key={recruit.id} className="hover:shadow-lg cursor-pointer">
-          <CardHeader className="w-full flex flex-row justify-between items-center">
-            <Badge>{recruit.party_type}</Badge>
-            <span className="text-sm text-gray-500 !mt-0">
-              {recruit.created_date_time}
-            </span>
-          </CardHeader>
-          <CardTitle className="text-center">{recruit.title}</CardTitle>
-          <CardFooter className="justify-center">
-            파티장: {recruit.created_by.username}
-          </CardFooter>
-        </Card>
-      ))}
-    </ul>
+    <>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {data?.map((recruit) => (
+          <RecruitCard key={recruit.id} recruit={recruit} />
+        ))}
+      </ul>
+    </>
   );
 };
 
