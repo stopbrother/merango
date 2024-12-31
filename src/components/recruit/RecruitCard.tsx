049815921bb8c7e5cf13ed5ab30f08cfc19943af
stopbrother <stopbro0323@naver.com>
2024-12-31 16:31:13 +0900

    feat: date-fns로 상대적 시간 표시

--- src/components/recruit/RecruitCard.tsx
+++ src/components/recruit/RecruitCard.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Card, CardFooter, CardHeader, CardTitle } from '../ui/card';
 import { Badge } from '../ui/badge';
 import { RecruitWithProfile } from '@/types/parties.types';
+import { formatDistanceToNow } from 'date-fns';
+import { ko } from 'date-fns/locale';
 
 interface RecruitCardProps {
   recruit: RecruitWithProfile;
   onClick: () => void;
 }
 const RecruitCard = ({ recruit, onClick }: RecruitCardProps) => {
+  const formattedTime = formatDistanceToNow(
+    new Date(recruit.created_date_time),
+    {
+      addSuffix: true,
+      locale: ko,
+    }
+  );
   return (
     <Card
       key={recruit.id}
@@ -16,9 +25,7 @@ const RecruitCard = ({ recruit, onClick }: RecruitCardProps) => {
     >
       <CardHeader className="w-full flex flex-row justify-between items-center">
         <Badge>{recruit.party_type}</Badge>
-        <span className="text-sm text-gray-500 !mt-0">
-          {recruit.created_date_time}
-        </span>
+        <span className="text-sm text-gray-500 !mt-0">{formattedTime}</span>
       </CardHeader>
       <CardTitle className="text-center">{recruit.title}</CardTitle>
       <CardFooter className="justify-center">
