ce30fdb404a9bc190f1f1f77eca66db71dc0dbab
stopbrother <stopbro0323@naver.com>
2024-12-31 16:31:48 +0900

    feat: 파티 참가 mutation 생성성

--- src/query/usePartyMutation.tsx
+++ src/query/usePartyMutation.tsx
@@ -1,6 +1,8 @@
-import { addRecruit } from '@/api/party-api';
+import { addMember, addRecruit } from '@/api/party-api';
+import { PartyMember } from '@/types/parties.types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+// 구인글
 export const useAddRecruitMutation = () => {
   const queryClient = useQueryClient();
 
@@ -9,6 +11,20 @@ export const useAddRecruitMutation = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['recruits'],
+      });
+    },
+  });
+};
+
+// 파티멤버
+export const useAddMemberMutation = (id: PartyMember['party_id']) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: addMember,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['members', id],
       });
     },
   });
