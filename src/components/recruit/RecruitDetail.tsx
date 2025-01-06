08412d61d90cad0bfd9b4af0dc9413a7e93553ad
stopbrother <stopbro0323@naver.com>
2025-01-06 18:19:54 +0900

    feat: 참가하기 기능 추가 (TODO: 중복참여 안되게 해야함)

--- src/components/recruit/RecruitDetail.tsx
+++ src/components/recruit/RecruitDetail.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import { useMembersQuery } from '@/query/member/useMembersQuery';
+import { useAddMemberMutation } from '@/query/member/useMembersMutation';
+import { RecruitWithProfile } from '@/types/parties.types';
+import { Button } from '../ui/button';
 import {
   Dialog,
   DialogContent,
@@ -6,14 +9,17 @@ import {
   DialogHeader,
   DialogTitle,
 } from '../ui/dialog';
-import { RecruitWithProfile } from '@/types/parties.types';
-import { Button } from '../ui/button';
 
 interface RecruitDetailProps {
   recruit: RecruitWithProfile;
   onClose: () => void;
 }
 const RecruitDetail = ({ recruit, onClose }: RecruitDetailProps) => {
+  const { data: members, error } = useMembersQuery(recruit.id);
+  if (error) console.log('error', error);
+
+  const { mutate: addMember } = useAddMemberMutation(recruit.id);
+
   return (
     <Dialog open onOpenChange={onClose}>
       <DialogContent>
@@ -21,13 +27,36 @@ const RecruitDetail = ({ recruit, onClose }: RecruitDetailProps) => {
           <DialogDescription>{recruit.party_type}</DialogDescription>
           <DialogTitle>{recruit.title}</DialogTitle>
         </DialogHeader>
-        <div>{recruit.description}</div>
+        <div className="mt-4">
+          <p className="text-gray-700">{recruit.description}</p>
+        </div>
 
-        <div>{/* TODO 참가중인 유저목록 */}유저목록</div>
+        <div className="mt-6">
+          <h3 className="text-lg font-bold mb-4">참가자 목록</h3>
+          <ul className="space-y-4">
+            {members?.map((member) => (
+              <li key={member.id} className="flex items-center gap-4">
+                <p className="text-gray-800 font-semibold">
+                  {member.profile_id.username}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-        <div>
-          <Button onClick={onClose}>참가 신청</Button>
-          <Button onClick={onClose}>닫기</Button>
+        <div className="mt-6 flex justify-center gap-4">
+          <Button
+            onClick={() => addMember(recruit.id)}
+            className="px-4 py-2 bg-green-600"
+          >
+            참가 신청
+          </Button>
+          <Button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-300 text-gray-800"
+          >
+            닫기
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
