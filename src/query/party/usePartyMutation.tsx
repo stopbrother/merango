ad0aafbf22a8f24c156dc74761e21bd0e0e634c1
stopbrother <stopbro0323@naver.com>
2025-03-21 15:58:31 +0900

    WIP: (Work In Progress) 구인글 삭제 진행중 (임시 저장)

--- src/query/party/usePartyMutation.tsx
+++ src/query/party/usePartyMutation.tsx
@@ -1,12 +1,30 @@
-import { addRecruit } from '@/api/party-api';
+import { addRecruit, deleteRecruit } from '@/api/party-api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 // 구인글
+
+// 구인글 등록
 export const useAddRecruitMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: addRecruit,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['recruits'],
+      });
+    },
+  });
+};
+
+// 구인글 업데이트
+
+// 구인글 삭제
+export const useDeleteRecruitMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteRecruit,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['recruits'],
