f3d4f7e6765a2ea536457c04984184381f8bdb26
stopbrother <stopbro0323@naver.com>
2025-04-09 16:14:42 +0900

    feat: 페이지 로딩 UI추가(스피너)

---
+++ src/app/profile/[id]/loading.tsx
@@ -0,0 +1,12 @@
+import Spinner from '@/components/ui/spinner';
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <Spinner size={40} />
+    </div>
+  );
+};
+
+export default loading;
