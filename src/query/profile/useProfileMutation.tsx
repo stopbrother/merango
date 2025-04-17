7d00b5c36471b80f47071887ec6a3e767844876f
stopbrother <stopbro0323@naver.com>
2025-04-17 14:55:24 +0900

    feat: 소개글 업데이트 성공/실패 메시지 toast 처리

--- src/query/profile/useProfileMutation.tsx
+++ src/query/profile/useProfileMutation.tsx
@@ -1,6 +1,7 @@
 import { updateUserIntro } from '@/api/profile-api';
 import { Profile } from '@/types/profiles.types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 interface useProfileMutationParams {
   intro: Profile['intro'];
@@ -13,9 +14,14 @@ export const useProfileMutation = (userId: Profile['id']) => {
     mutationFn: ({ intro }: useProfileMutationParams) =>
       updateUserIntro(intro, userId),
     onSuccess: () => {
+      toast.success('프로필 소개가 업데이트 되었습니다.');
+
       queryClient.invalidateQueries({
         queryKey: ['userProfile', userId],
       });
+    },
+    onError: () => {
+      toast.error('업데이트에 실패했습니다.');
     },
   });
 };
