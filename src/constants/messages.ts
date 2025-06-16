858b767603de6340a83121545d76453d00a63d3b
stopbrother <stopbro0323@naver.com>
2025-06-16 15:14:15 +0900

    feat: zod validation message 분리

---
+++ src/constants/messages.ts
@@ -0,0 +1,12 @@
+export const MSG = {
+  USERNAME: {
+    MIN: '닉네임은 최소 2자 이상이어야 합니다.',
+  },
+  SOCIAL: {},
+  LEVEL: {
+    TYPE: '숫자를 입력해 주세요.',
+    MIN: '1 이상이어야 합니다.',
+    MAX: '200 이하이어야 합니다.',
+  },
+  JOB: {},
+};
