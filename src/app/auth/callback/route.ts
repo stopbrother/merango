291721c5add504af52d4a65513b813c910b206a9
stopbrother <stopbro0323@naver.com>
2025-06-17 20:17:12 +0900

    refactor: 첫로그인만 디스코드 이후에는 플랫폼내에서 관리( handle_new_user 트리거)

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,17 +2,6 @@ import { NextResponse } from 'next/server';
 // The client you created from the Server-Side Auth instructions
 import { createClient } from '@/utils/supabase/server';
 
-// Discord API 사용자 정보
-async function fetchDiscordProfile(accessToken: string) {
-  const response = await fetch('https://discord.com/api/users/@me', {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  if (!response.ok) throw new Error('디스코드 응답 에러');
-  return response.json();
-}
-
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
@@ -22,28 +11,8 @@ export async function GET(request: Request) {
   if (code) {
     const supabase = await createClient();
     // Authorization code를 통한 세션 교환
-    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const user = data.session;
-      const accessToken = data.session.provider_token;
-
-      if (accessToken) {
-        try {
-          // Discord API로 사용자 정보 가져오기
-          const discordProfile = await fetchDiscordProfile(accessToken);
-          const global_name = discordProfile.global_name;
-
-          if (!global_name) return;
-          // username 업데이트
-          await supabase
-            .from('profiles')
-            .update({ username: global_name })
-            .eq('id', user.user.id);
-        } catch (err) {
-          console.error(err);
-        }
-      }
-
       const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development';
       if (isLocalEnv) {
