61bb528b0ac40b9e5bd1a463dc43b47b039ec5a9
stopbrother <stopbro0323@naver.com>
2025-06-23 19:57:20 +0900

    refactor: 쿼리파라미터로 탭 변경

--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
 import CreatedParties from './CreatedParties';
 import JoiningParty from './JoiningParty';
@@ -10,17 +10,26 @@ interface ProfileTabsProps {
 }
 
 const ProfileTabs = ({ userId }: ProfileTabsProps) => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const tab = searchParams.get('tab') ?? 'intro';
 
+  // 탭변경 핸들러
+  const handleTabsChange = (value: string) => {
+    if (tab !== value)
+      router.replace(
+        `/profile/${userId}${value === 'intro' ? '' : `?tab=${value}`}`
+      );
+  };
+
   return (
-    <Tabs defaultValue={tab}>
+    <Tabs value={tab} onValueChange={handleTabsChange}>
       <TabsList className="border-b">
         <TabsTrigger value="intro" variant="underline">
           소개
         </TabsTrigger>
         <TabsTrigger value="joined" variant="underline">
-          참가 중인 파티
+          참가중인 파티
         </TabsTrigger>
         <TabsTrigger value="created" variant="underline">
           생성한 파티
