37fd9337fb608003ab68d8ce067cd69acfc834c5
stopbrother <stopbro0323@naver.com>
2025-06-28 13:05:59 +0900

    refactor: Header SSR 전환 및 AuthHeader 분리, 상태별 렌더링 개선

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,9 @@
-'use client';
 import Link from 'next/link';
 
-import { useAuthQuery } from '@/query/auth/useAuthQuery';
-import { useProfileQuery } from '@/query/profile/useProfileQuery';
-import LoginButton from './auth/LoginButton';
-import UserDropdownButton from './auth/UserDropdownButton';
+import AuthHeader from './auth/AuthHeader';
 import RecruitButton from './RecruitButton';
 
 const Header = () => {
-  // auth 테이블 조회
-  const { data: user } = useAuthQuery();
-
-  // 타입 에러 방지 (enabled로 실행 제어됨)
-  const userId = user?.id ?? '';
-
-  // profiles 테이블 조회
-  const { data: profile } = useProfileQuery(userId);
-
   return (
     <header className="w-full sticky top-0 bg-[#588157]">
       <div className="max-w-[1200px] h-[60px] flex justify-between items-center mx-auto px-4">
@@ -31,8 +18,8 @@ const Header = () => {
 
         <div className="flex items-center gap-6">
           <RecruitButton />
-          {profile ? <UserDropdownButton profile={profile} /> : <LoginButton />}
-          {/* <AuthHeader /> */}
+          <AuthHeader />
+          {/* {profile ? <UserDropdownButton profile={profile} /> : <LoginButton />} */}
         </div>
       </div>
     </header>
