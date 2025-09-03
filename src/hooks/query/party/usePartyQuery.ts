d780a76ea861f257c7b3be8d69adbdb0837235a9
stopbrother <stopbro0323@naver.com>
2025-09-03 13:09:59 +0900

    refactor: supabase client 싱글톤으로 변경

--- src/hooks/query/party/usePartyQuery.ts
+++ src/hooks/query/party/usePartyQuery.ts
@@ -4,13 +4,11 @@ import {
   getInfiniteParties,
   getParties,
 } from '@/api/party-api';
-import { createClient } from '@/utils/supabase/client';
+import { browserClient } from '@/utils/supabase/client';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 // 구인글(파티) 리스트 조회
 export const usePartiesQuery = (partyType: string, keyword?: string) => {
-  const browserClient = createClient();
-
   return useQuery({
     queryKey: ['recruits', partyType, keyword],
     queryFn: () => getParties(browserClient, partyType, keyword),
@@ -19,8 +17,6 @@ export const usePartiesQuery = (partyType: string, keyword?: string) => {
 
 // 생성한 구인글(파티) 조회
 export const useCreatedPartiesQuery = (userId: string) => {
-  const browserClient = createClient();
-
   return useQuery({
     queryKey: ['createdParties', userId],
     queryFn: () => getCreatedParties(browserClient, userId),
@@ -29,8 +25,6 @@ export const useCreatedPartiesQuery = (userId: string) => {
 
 // 생성한 구인글(파티) 개수조회 (count)
 export const useCreatedPartiesCountQuery = (userId: string) => {
-  const browserClient = createClient();
-
   return useQuery({
     queryKey: ['createdParties', 'count', userId],
     queryFn: () => getCreatedPartiesCount(browserClient, userId),
@@ -42,8 +36,6 @@ export const useInfinitePartiesQuery = (
   partyType: string,
   keyword?: string
 ) => {
-  const browserClient = createClient();
-
   return useInfiniteQuery({
     queryKey: ['recruits', partyType, keyword],
     queryFn: ({ pageParam }) =>
