5b31f24aca6760bda8cf668c1d3f4901d230af04
stopbrother <stopbro0323@naver.com>
2025-11-19 15:29:54 +0900

    chore: 소개글 변경 (검색 노출 향상을 위한)

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
       {/* 제목 & 파티찾기 버튼 */}
       <div className="flex flex-col items-center gap-6">
         <h1 className="text-4xl font-bold text-gray-800">
-          MapleLand 파티 매칭
+          메이플랜드 파티 매칭
         </h1>
 
         {/* 소개글 */}
         <p className="text-sm text-muted-foreground text-center max-w-xl">
-          메랜고(MeranGo)는 MapleLand 유저를 위한 메이플랜드 파티 매칭
-          서비스입니다. 메랜파티를 구인·구직하고 싶다면, 디스코드 로그인만으로
+          메랜고(MeranGo)는 MapleLand 유저를 위한 메이플랜드 파티 구인·구직
+          플랫폼입니다. 메랜파티를 구인·구직하고 싶다면, 디스코드 로그인만으로
           간편하게 원하는 파티를 찾고 모집해 보세요.
         </p>
 
