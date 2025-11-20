bf28007c4e82741ca63eae83dc0fb7eabddfcafb
stopbrother <stopbro0323@naver.com>
2025-11-20 13:40:59 +0900

    refactor: 메타데이터 제목 한글로 변경 및 png파일 파비콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,21 +14,24 @@ const pretendard = localFont({
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
 
 export const metadata: Metadata = {
-  // 페이지에 title이 있을경우 %s 자리에 title → (title - MeranGo)
+  // 페이지에 title이 있을경우 %s 자리에 title → (title - 메랜고)
   title: {
-    template: 'MeranGo - %s',
-    default: 'MeranGo - 메이플랜드 파티 매칭',
+    template: '메랜고 - %s',
+    default: '메랜고 - 메이플랜드 파티 매칭',
   },
   description:
     '메이플랜드 파티 매칭 - 디스코드 로그인으로 간편하게 구인/구직 하세요, 메랜고',
   // metadata내 상대경로를 절대 경로로 바꾸는 기준 주소
   metadataBase: new URL(BASE_URL),
-  icons: { icon: '/favicon.svg', apple: '/apple-icon' },
+  icons: {
+    icon: ['/favicon.svg', '/favicon-48x48.png'],
+    apple: '/apple-icon',
+  },
   // 미리보기 카드에서 쓰임 (sns/메신저)
   openGraph: {
     url: BASE_URL,
     type: 'website',
-    title: 'MeranGo',
+    title: '메랜고',
     siteName: 'MeranGo',
     description:
       '메이플랜드 파티 매칭 - 디스코드 로그인으로 간편하게 구인/구직 하세요',
